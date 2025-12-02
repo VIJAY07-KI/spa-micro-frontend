e0269579ea4d9bcd28be1251a49ffd4cbb5258d6
VIJAY07-KI <bandapallivijay1612@gmail.com>
2025-12-02 14:43:41 +0530

    local sucess

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,17 +1,23 @@
-import React from "react";
-import StoryApp from "story_app/App";
-import CommentsApp from "comments_app/App";
+import React, { Suspense, lazy } from "react";
+
+// Lazy load remote apps
+const StoryApp = lazy(() => import("story_app/App"));
+const CommentsApp = lazy(() => import("comments_app/App"));
 
 export default function App() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>Micro Frontend Host</h1>
+      <h1 style={{ textAlign: "center" }}> SPA Micro Frontend Host</h1>
 
-      <h2>📚 Story App</h2>
-      <StoryApp />
+      <h2 style={{ textAlign: "center" }}>📚 Story App</h2>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading Story App...</p>}>
+        <StoryApp />
+      </Suspense>
 
-      <h2>💬 Comments App</h2>
-      <CommentsApp />
+      <h2 style={{ textAlign: "center" }}>💬 Comments App</h2>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading Comments...</p>}>
+        <CommentsApp />
+      </Suspense>
     </div>
   );
 }
